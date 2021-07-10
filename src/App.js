e84d3820dc5d8f75a272fc619806f2b1d4f97161
Yandiswa-30 <yandiswandarane442@gmail.com>
2021-07-10 22:03:20 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ export default function App() {
     <div>
       <h1>Hello Sta!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Grid container spacing={1}>
+  <Grid container item xs={12} spacing={3}>
+    <FormRow />
+  </Grid>
+  <Grid container item xs={12} spacing={3}>
+    <FormRow />
+  </Grid>
+  <Grid container item xs={12} spacing={3}>
+    <FormRow />
+  </Grid>
+</Grid>
     </div>
+       
   );
 }
